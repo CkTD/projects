ce02489d4d1c8785f4f193d0bb0862eb1e4ee6b0
lawrence <lawrence@localhost.localdomain>
2019-06-05 21:54:26 +0800

    changes

--- dict/install.sh
+++ dict/install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 DICT_DIR=$HOME/.dict
-INSTALL_DIR=$HOME/bin
-ALL="dictconf.py dict note _dict.py"
+INSTALL_DIR=$HOME/.local/
+ALL="dictconf.py dict _dict.py"
 
 
 if ! [ -d $DICT_DIR ]; then
     mkdir -p $DICT_DIR
 fi
 
-install $ALL $INSTALL_DIR
+INSTALL_BIN=${INSTALL_DIR}bin
+INSTALL_LIB=${INSTALL_DIR}lib/dict
+
+if ! [ -d $INSTALL_LIB ]; then
+    mkdir -p $INSTALL_LIB
+fi
+
+cp $ALL $INSTALL_LIB
+ln -s ${INSTALL_LIB}/dict ${INSTALL_BIN}/dict
